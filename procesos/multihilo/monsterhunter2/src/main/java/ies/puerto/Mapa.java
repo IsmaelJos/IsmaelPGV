8764dd4f5a5af75f99feb2239d1f1726f75e8401
IsmaelJos <isamelejano@gmail.com>
2024-10-15 11:05:42 +0100

    feat:

---
+++ procesos/multihilo/monsterhunter2/src/main/java/ies/puerto/Mapa.java
@@ -0,0 +1,82 @@
+package ies.puerto;
+
+public class Mapa {
+    private String[][] map;
+    private static int size = 5;
+
+    public Mapa() {
+        this.map = new String[size][size]; 
+        generarMapa();
+    }
+    private void generarMapa() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                this.map[i][j] = " * "; 
+            }
+        }
+    }
+
+    public void showMapa(){
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(map[i][j]);
+            }
+            System.out.println(" ");
+        }
+        System.out.println(" ");
+    }
+
+    public synchronized void addMonstruo() {
+        int x = (int) (Math.random() * size);
+        int y = (int) (Math.random() * size);
+        if (this.map[x][y].equals(" * ")) {
+            map[x][y] = " M ";
+        }else{
+            addMonstruo();
+        }
+
+    }
+
+
+    public synchronized void addCazador(Cazador cazador) {
+        int x = cazador.getPosX();
+        int y = cazador.getPosY();
+        if (this.map[x][y].equals(" * ")) {
+            map[x][y] = " C ";
+            cazador.setPos(x,y);
+        }else{
+            addCazador(cazador);
+        }
+
+    }
+
+    public synchronized boolean moverCazador(Cazador cazador) {
+        int x = (int) (Math.random() * size);
+        int y = (int) (Math.random() * size);
+        if (this.map[x][y].equals(" * ")) {
+            map[x][y] = " C ";
+            this.map[cazador.getPosX()][cazador.getPosY()] = " * ";
+            cazador.setPos(x,y);
+            return false;
+            
+        }if(this.map[x][y].equals(" M ")){
+            cazador.atraparMonstruo();
+            map[x][y] = " C ";
+            this.map[cazador.getPosX()][cazador.getPosY()] = " * ";
+            cazador.setPos(x,y);
+            this.showMapa();
+            return true;
+            
+        }if (this.map[x][y].equals(" C ")) {
+            moverCazador(cazador);
+            return false;
+        }
+        return false;
+        
+    }
+
+    public int getSize(){
+        return size;
+    }
+    
+}
---
+++ procesos/multihilo/monsterhunter2/src/main/java/ies/puerto/Monstruo.java
@@ -0,0 +1,12 @@
+package ies.puerto;
+
+import java.util.Random;
+
+public class Monstruo{
+
+    private String nombre;
+    
+    public Monstruo(String nombre) {
+        this.nombre = nombre;
+    }
+}
