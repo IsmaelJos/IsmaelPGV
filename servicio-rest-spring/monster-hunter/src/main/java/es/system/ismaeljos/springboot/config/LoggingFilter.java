353693eae79aefb8ed4a8a4cf1efe924b65d6774
IsmaelJos <ismaelejano@gmail.com>
2025-02-05 16:21:51 +0000

    feat: relacion entre tablas

---
+++ servicio-rest-spring/monster-hunter/src/main/java/es/system/ismaeljos/springboot/config/LoggingFilter.java
@@ -0,0 +1,25 @@
+package es.system.ismaeljos.springboot.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+public class LoggingFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.info("Request URL: {}", request.getRequestURI());
+
+        logger.info("Request Headers:");
+        request.getHeaderNames().asIterator().forEachRemaining(headerName ->{
+            log.info("{}: {}", headerName, request.getHeader(headerName));
+        });
+
+        filterChain.doFilter(request, response);
+    }
+}
